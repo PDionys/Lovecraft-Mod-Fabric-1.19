9105f318bc19433850e39e7451a72930e16c30da
PDionys <p.deniskpi@gmail.com>
2024-06-09 12:53:36 +0300

    Initial commit

---
+++ src/main/java/net/pdeins/lovecraftmod/client/LovecraftModClient.java
@@ -0,0 +1,10 @@
+package net.pdeins.lovecraftmod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LovecraftModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
