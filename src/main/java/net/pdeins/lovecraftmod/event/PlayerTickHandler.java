e6bf7ab644d2a1eea9f98f845b18c4add38e2924
PDionys <p.deniskpi@gmail.com>
2024-06-11 10:07:14 +0300

    SANITY v4.1 MORE LOOKING LOGIC

--- src/main/java/net/pdeins/lovecraftmod/event/PlayerTickHandler.java
+++ src/main/java/net/pdeins/lovecraftmod/event/PlayerTickHandler.java
@@ -1,10 +1,8 @@
 package net.pdeins.lovecraftmod.event;
 
-import net.fabricmc.fabric.api.entity.FakePlayer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.passive.CowEntity;
-import net.minecraft.entity.passive.PigEntity;
+import net.minecraft.entity.mob.*;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
@@ -15,10 +13,8 @@ import net.pdeins.lovecraftmod.util.IEntityDataSaver;
 import net.pdeins.lovecraftmod.util.RaycastEntity;
 import net.pdeins.lovecraftmod.util.SanityData;
 
-import java.util.Random;
-
 public class PlayerTickHandler implements ServerTickEvents.StartTick{
-    private static int tickCount = 0;
+    private static int darkTickCount, mobTickCount = 0;
 
     @Override
     public void onStartTick(MinecraftServer server) {
@@ -26,7 +22,7 @@ public class PlayerTickHandler implements ServerTickEvents.StartTick{
             //TODO check how it work in online
             SanityData.syncSanity(((IEntityDataSaver) player).getPersistentData().getInt("sanity"), player);
             // Method for decrease sanity in the dark
-            sanytiDecreaseInTheDark(player);
+            sanityDecreaseInTheDark(player);
             // Method for decrease sanity when looking on target mob
             sanityDecreaseLookingOnMob(player);
         }
@@ -34,22 +30,31 @@ public class PlayerTickHandler implements ServerTickEvents.StartTick{
 
     private static void sanityDecreaseLookingOnMob(ServerPlayerEntity player) {
         //Method which return entity on raycast
-        EntityHitResult hitResult = RaycastEntity.raycastEntity(player, 100);
+        EntityHitResult hitResult = RaycastEntity.raycastEntity(player, 250); // in this case 250 = 16 blocks away
         // Check if return is not null
         if(hitResult != null) {
             // Get entity from hit
             Entity entity = hitResult.getEntity();
             // Check if entity is needed target(s)
-            if(entity instanceof CowEntity || entity instanceof PigEntity){
-                // Decrease sanity if player watch on target
-                player.sendMessage(Text.literal("Its a cow or pig!!!"));
+            boolean fearTarget = entity instanceof ZombieEntity
+                    || entity instanceof SkeletonEntity
+                    || entity instanceof SpiderEntity
+                    || entity instanceof CreeperEntity
+                    || entity instanceof EndermanEntity;
+            if(fearTarget){// Decrease sanity if player watch on target
+                //grow tick count
+                mobTickCount++;
                 //TODO make normal condition for decrease
-                decreaseSanity(player);
-            }else player.sendMessage(Text.literal("Its a NOT cow!!!"));
-        }
+                if (mobTickCount >= 20){ // after 1s looking at mob
+                    // decrease sanity level by amount
+                    decreaseSanity(player);
+                    mobTickCount = 0;
+                }
+            }else mobTickCount = 0; // if its a mob but not fearful reset ticks
+        }else mobTickCount = 0;// if mob hasnt been detected tick reset
     }
 
-    private static void sanytiDecreaseInTheDark(ServerPlayerEntity player) {
+    private static void sanityDecreaseInTheDark(ServerPlayerEntity player) {
         //Set player position
         BlockPos playerPos = player.getBlockPos();
         //Set time and light level
@@ -59,15 +64,15 @@ public class PlayerTickHandler implements ServerTickEvents.StartTick{
         if(lightLevel <= 7 || (lightLevel <= 15 && (time >= 13000 && time <= 23000))){
             // condition when player in dark or night and dark
             //grow tick count
-            tickCount++;
+            darkTickCount++;
             //check if went 1 minute
-            if(tickCount >= 1200){// 1200 ticks = 1 minute
+            if(darkTickCount >= 160){//darkTickCount >= 1200 - 1200 ticks = 1 minute (20 ticks - 1s)
                 // decrease sanity level by amount
                 decreaseSanity(player);
                 //reset tick count
-                tickCount = 0;
+                darkTickCount = 0;
             }
-        }else  tickCount = 0; // if day or light count = 0
+        }else  darkTickCount = 0; // if day or light count = 0
     }
 
     private static void decreaseSanity(ServerPlayerEntity player) {
